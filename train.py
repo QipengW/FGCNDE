673d0e7037d2cff1c2cfccb9686703a6bc861dbc
QipengW <2693479453@qq.com>
2024-10-22 14:04:29 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,23 +6,21 @@ criterion = nn.MSELoss()
 train_loss = []
 val_loss = []
 test_loss = []
-optimizer = optim.Adam(params=my_net.parameters(),lr=0.0003)
+optimizer = optim.Adam(params=my_net.parameters(),lr=0.0001)
 Epoch = 3000
-min_loss = 100000
 import torch.nn.functional as F
 my_net.train()
 for epoch in range(Epoch):
     my_net.train()
     for data in train_loader:# ["graph": [B, N, N] , "flow_x": [B, N, H, D], "flow_y": [B, N, 1, D]]
         my_net.zero_grad( )
-        predict_value,zero,ceshi,V,gate,res_1,res_2,res_3,res_4,res_5,res_6,res_7,res_8,step_0,step_1 = my_net(data,A,device)
-              
-        
-        loss = criterion(predict_value, data["flow_y"])+criterion(zero, ceshi)+criterion(zero,V+gate)
+        pre,zero,ceshi,V0,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,gate,res_1,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12 = my_net(data,device)
+        loss = criterion(pre, data["flow_y"])+0.01*criterion(zero, ceshi)+0.01*(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12
+                                                                                          +V0+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11)
         loss.backward()
         optimizer.step()
         train_loss.append(loss.item())
 
     if epoch%100==0:
-        torch.save(my_net,'./model_FGCNDE_PEMS04_ceshi'+'.pth')
+        torch.save(my_net,'./model_SFGDE'+'.pth')
         print("Epoch: {:04d},train_loss: {:02.8f}".format(epoch,loss)) 
